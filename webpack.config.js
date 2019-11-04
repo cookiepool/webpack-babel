d1f6bc466a3cdfc6f6e51d17ea05830ae82bdd92
haol <1953312641@qq.com>
2019-11-04 17:58:14 +0800

    '++'

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    // 指定模式（这个指定可以用来区分开发模式和生产模式，可选值有：'devvlopment','production','none'）
+    mode: 'none',
+    // 入口
+    entry: {
+        main: path.resolve(__dirname, './src/index.js')
+    },
+
+    // 出口
+    output: {
+        // 出口文件
+        path: path.resolve(__dirname, './dist'),
+        // 文件名
+        filename: './[name].js',
+    },
+}
